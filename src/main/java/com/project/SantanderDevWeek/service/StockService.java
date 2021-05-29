aa39495599738ed71a5ca8dfee515ac4c25e6b00
anderrf <anderson_rocha_farias@outlook.com>
2021-05-28 22:28:09 -0300

    Exception handlers

--- src/main/java/com/project/SantanderDevWeek/service/StockService.java
+++ src/main/java/com/project/SantanderDevWeek/service/StockService.java
@@ -1,12 +1,16 @@
 package com.project.SantanderDevWeek.service;
 
+import com.project.SantanderDevWeek.exceptions.BusinessException;
 import com.project.SantanderDevWeek.mapper.StockMapper;
 import com.project.SantanderDevWeek.model.Stock;
 import com.project.SantanderDevWeek.model.dto.StockDTO;
 import com.project.SantanderDevWeek.repository.StockRepository;
+import com.project.SantanderDevWeek.util.MessageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class StockService {
@@ -19,6 +23,10 @@ public class StockService {
 
     @Transactional
     public StockDTO save(StockDTO dto) {
+        Optional<Stock> optionalStock = repository.findByNameAndDate(dto.getName(), dto.getDate());
+        if(optionalStock.isPresent()){
+            throw new BusinessException(MessageUtils.STOCK_ALREADY_EXISTS);
+        }
         Stock stock = mapper.toEntity(dto);
         repository.save(stock);
         //dto.setId(stock.getId());
---
+++ src/main/java/com/project/SantanderDevWeek/util/MessageUtils.java
@@ -0,0 +1,7 @@
+package com.project.SantanderDevWeek.util;
+
+public abstract class MessageUtils {
+
+    public static final String STOCK_ALREADY_EXISTS = "Stock already exists in the database";
+
+}
